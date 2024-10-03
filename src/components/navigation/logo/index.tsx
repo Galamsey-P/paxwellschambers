6effc28122315fd7edb3946dfb6a3b02c8e98426
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-10-03 12:34:21 +0000

    feat: logo component with variants

---
+++ src/components/navigation/logo/index.tsx
@@ -0,0 +1,23 @@
+import { ClassValue } from "clsx";
+import { LogoVariant, logoVariantMap } from "./index.types";
+import { cn } from "@/lib/utils";
+import { useNavigation } from "@/context/navigation";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import Image from "next/image";
+
+type Props = {
+    variant?: LogoVariant
+    className?: ClassValue;
+};
+
+export default function Logo({ className, variant }: Props) {
+    const { logoVariant } = useNavigation();
+
+    return (
+        <div className={cn("w-[30px] md:w-[40px] 2xl:w-[52px]", className)}>
+            <AspectRatio ratio={52 / 78}>
+                <Image src={`/images/${logoVariantMap[variant || logoVariant]}`} alt={`${logoVariant} logo`} width={52} height={78} className={cn("", className)} priority />
+            </AspectRatio>
+        </div>
+    );
+}
---
+++ src/components/navigation/logo/index.types.ts
@@ -0,0 +1,8 @@
+
+// keys refer to the theme
+export const logoVariantMap = {
+    "white": "white-logo.svg",
+    "colored": "colored-logo.svg",
+}
+
+export type LogoVariant = keyof typeof logoVariantMap
