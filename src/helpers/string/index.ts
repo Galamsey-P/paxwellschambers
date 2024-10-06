f34bc0f20df0376d9933eb4ac95e0ee99c5e9803
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-10-06 12:46:44 +0000

    feat: getInitials function

--- src/helpers/string/index.ts
+++ src/helpers/string/index.ts
@@ -12,3 +12,16 @@ export function sluggify(word?: string) {
     const modifiedWord = __word?.toString()?.replace(/[^a-zA-Z0-9]/g, '-');
     return modifiedWord.toLowerCase();
 }
+
+export function getInitials(word?: string): string {
+    const __word = stringify(word)
+
+    const names = __word.split(' ')
+    let initials = names[0].substring(0, 1).toUpperCase();
+
+    if (names.length > 1) {
+        initials += names[names.length - 1].substring(0, 1).toUpperCase();
+    }
+    return initials;
+    
+}
