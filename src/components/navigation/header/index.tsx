03ea34c1322df92380b929433ecbef1bc620209f
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-10-06 12:47:57 +0000

    style: enhance styling of transparent navbar

--- src/components/navigation/header/index.tsx
+++ src/components/navigation/header/index.tsx
@@ -12,15 +12,19 @@ import Logo from "../logo";
 
 
 export default function Header() {
-    const {isTransparent} = useNavigation()
+    const { isTransparent } = useNavigation();
     return (
-        <header className={cn(
-            " py-4 sticky top-0 z-50 w-full font-sans",
-            isTransparent ? "bg-transparent" : "bg-white"
-        )}>
+        <header
+            className={cn(
+                "py-4 sticky top-0 z-50 w-full font-sans transition-all duration-300",
+                isTransparent
+                    ? "bg- backdrop-blur-md bg-black/10"
+                    : "bg-white shadow"
+
+            )}>
             <div className="container flex items-center justify-between mx-auto px-2">
-                <Logo  />
-                <DesktopNav  />
+                <Logo />
+                <DesktopNav />
                 <div className="md:flex hidden">
                     <Link href="/contact">
                         <Button>
