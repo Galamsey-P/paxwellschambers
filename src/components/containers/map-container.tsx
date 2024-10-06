4bf47e46319ab4d46d98c2650a1e734d86b370e5
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-10-06 17:01:00 +0000

    feat: embed map

---
+++ src/components/containers/map-container.tsx
@@ -0,0 +1,13 @@
+export default function MapContainer() {
+    return (
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.7367882124963!2d-0.22808892432829725!3d5.605842994375059!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf997fc9acbe03%3A0x2b390592bc59994f!2sDubik%20%26%20Associates%20-%20Pax%20%26%20Wells%20Chambers%2C%20(Legal%20Practitioners%20%26%20Notary%20Public)!5e0!3m2!1sen!2sgh!4v1728230800224!5m2!1sen!2sgh"
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            className="border-0 w-screen min-h-screen"
+        >
+
+        </iframe>
+    )
+}
