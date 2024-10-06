42da5bed650d99781d245db6a943580977af4415
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-10-06 12:48:17 +0000

    refactor: remove unused variables

--- src/components/navigation/data.ts
+++ src/components/navigation/data.ts
@@ -12,22 +12,3 @@ export const PAGES: { title: string; href: string;  }[] = [
         href: "/practices"
     },
 ];
-
-export const MOBILE_PAGES: { title: string; href: string; }[] = [
-    {
-        title: "Home",
-        href: "/"
-    },
-    {
-        title: "About Us",
-        href: "/about"
-    },
-    {
-        title: "Our Practices",
-        href: "/practices"
-    },
-    {
-        title: "Contact Us",
-        href: "/contact"
-    },
-];
