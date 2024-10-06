87319db1b65ea111f931fd7c518591e2a894188f
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-10-06 12:46:25 +0000

    feat: store static data

---
+++ src/lib/data.ts
@@ -0,0 +1,94 @@
+export const PRACTICES = [
+    "Family Law",
+    "Health Law",
+    "Business Law",
+    "Sports Law",
+    "Family Law",
+    "Health Law",
+    "Business Law",
+    "Sports Law",
+    "Legal Service",
+    "Legal Service",
+];
+
+
+export const TESTIMONIALS = [
+    {
+        image: "kuf.jpeg",
+        name: "John Agyekum Kuffuor",
+        position: "Ex-President, Republic Of Ghana",
+        quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        image: "kuf.jpeg",
+        name: "Kofi Annan",
+        position: "Former UN Secretary-General",
+        quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        image: "kuf.jpeg",
+        name: "Kwame Nkrumah",
+        position: "First President of Ghana",
+        quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        image: "kuf.jpeg",
+        name: "John Agyekum Kuffuor",
+        position: "Ex-President, Republic Of Ghana",
+        quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        image: "kuf.jpeg",
+        name: "Abedi Pele",
+        position: "Former Ghanaian Footballer",
+        quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        image: "kuf.jpeg",
+        name: "John Agyekum Kuffuor",
+        position: "Ex-President, Republic Of Ghana",
+        quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        image: "kuf.jpeg",
+        name: "John Agyekum Kuffuor",
+        position: "Ex-President, Republic Of Ghana",
+        quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        image: "kuf.jpeg",
+        name: "Efua Sutherland",
+        position: "Ghanaian Playwright",
+        quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        image: "kuf.jpeg",
+        name: "Ayi Kwei Armah",
+        position: "Ghanaian Novelist",
+        quote: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+];
+
+
+export const CLIENTS = [
+    {
+        name: "ProLine",
+        image: "clients/proline.png"
+    },
+    {
+        name: "DevWise",
+        image: "clients/devwise.png"
+    },
+    {
+        name: "Flash",
+        image: "clients/flash.png"
+    },
+    {
+        name: "HiTech",
+        image: "clients/hitech.png"
+    },
+    {
+        name: "Invert",
+        image: "clients/invert.png"
+    },
+]
