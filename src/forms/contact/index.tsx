c7b1c1a65bf929a2002e25c875a364a1086cf273
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-10-06 16:57:59 +0000

    feat: add contact form

---
+++ src/forms/contact/index.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { toast } from "sonner";
+import schema from "./schema";
+
+
+
+export default function ContactForm() {
+    const form = useForm<z.infer<typeof schema>>({
+        resolver: zodResolver(schema),
+        defaultValues: {
+            name: "",
+            email: "",
+            message: "",
+        },
+    });
+
+    function onSubmit(values: z.infer<typeof schema>) {
+        console.log(values);
+        toast.success("Your request has been sent!");
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 flex flex-col gap-8">
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Name <span className="text-yellow-700">*</span></FormLabel>
+                            <FormControl>
+                                <Input placeholder="Your name" {...field} className="bg-transparent border-b border-gray-300 focus:border-transparent placeholder:text-white/80" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email <span className="text-yellow-700">*</span></FormLabel>
+                            <FormControl>
+                                <Input placeholder="Your email" {...field} className="bg-transparent border-b border-gray-300 focus:border-transparent placeholder:text-white/80" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="message"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Message <span className="text-yellow-700">*</span></FormLabel>
+                            <FormControl>
+                                <Textarea placeholder="Your message" {...field} className="bg-transparent border-none resize-none border-gray-300 focus:border-transparent placeholder:text-white/80" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit" variant={"primary"}>Submit</Button>
+            </form>
+        </Form>
+    );
+}
---
+++ src/forms/contact/schema.ts
@@ -0,0 +1,16 @@
+import * as z from "zod";
+
+
+const schema = z.object({
+    name: z.string().min(2, {
+        message: "Name must be at least 2 characters.",
+    }),
+    email: z.string().email({
+        message: "Please enter a valid email address.",
+    }),
+    message: z.string().min(10, {
+        message: "Message must be at least 10 characters.",
+    }),
+});
+
+export default schema
