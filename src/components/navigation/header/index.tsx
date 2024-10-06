bcf264091a51b30cc2e8e1bd696328ea5ec22090
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-10-06 17:58:39 +0000

    feat: on scroll darken the transparent header

--- src/components/navigation/header/index.tsx
+++ src/components/navigation/header/index.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 import { cn } from "@/lib/utils";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DesktopNav from "./desktop";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
@@ -13,15 +13,27 @@ import Logo from "../logo";
 
 export default function Header() {
     const { isTransparent } = useNavigation();
+    const [hasScrolled, setHasScrolled] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            setHasScrolled(scrollPosition > 800); 
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
     return (
         <header
             className={cn(
                 "py-4 sticky top-0 z-50 w-full font-sans transition-all duration-300",
                 isTransparent
-                    ? "bg- backdrop-blur-md bg-black/10"
+                    ? hasScrolled
+                        ? "bg-black/70 backdrop-blur-md"
+                        : "bg- backdrop-blur-md bg-black/30"
                     : "bg-white shadow"
-
-            )}>
+            )}
+        >
             <div className="container flex items-center justify-between mx-auto px-2">
                 <Logo />
                 <DesktopNav />
