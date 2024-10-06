ae897aee67135a2cdb7532155059c7f0016db61f
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-10-06 12:48:53 +0000

    feat: automatically detect aspect ratio of image

--- src/components/image/absolute.tsx
+++ src/components/image/absolute.tsx
@@ -1,6 +1,8 @@
 import { cn } from "@/lib/utils";
 import { ClassValue } from "clsx";
 import Image from "next/image";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+
 
 type Props = {
     src: string;
@@ -8,18 +10,23 @@ type Props = {
     style: ClassValue;
     object?: "contain" | "cover";
     priority?: boolean;
-
+    width: number; // original width
+    height: number; // original height
 };
 
-export default function AbsoluteImage({ src, alt, style, object, priority}: Props) {
+export default function AbsoluteImage({ src, alt, style, object, priority, height, width }: Props) {
     return (
         <div className={cn("absolute ", style)}>
-            <Image
-                fill
-                alt={alt || ""}
-                src={`/images/${src}`}
-                priority={priority || false}
-                className={cn('object-contain', object === "cover" && "object-cover")} />
+            <AspectRatio ratio={width/height}>
+                <Image
+                    draggable={false}
+                    width={width}
+                    height={height}
+                    alt={alt || ""}
+                    src={`/images/${src}`}
+                    priority={priority || false}
+                    className={cn('object-contain', object === "cover" && "object-cover")} />
+            </AspectRatio>
         </div>
     );
 }
--- src/components/image/relative.tsx
+++ src/components/image/relative.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { ClassValue } from "clsx";
 import { CSSProperties } from "react";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { stringify } from "@/helpers/string";
 
 type Props = {
@@ -12,26 +13,33 @@ type Props = {
     innerStyles?: ClassValue;
     priority?: boolean;
     containerHTMLStyles?: CSSProperties;
+    width: number; // original width
+    height: number; // original height
 };
 
-export default function RelativeImage({ containerStyles, src, alt, innerStyles, object, priority, containerHTMLStyles,  }: Props) {
+export default function RelativeImage({ containerStyles, src, alt, innerStyles, object, priority, containerHTMLStyles, height, width}: Props) {
     return (
-        <div className={cn("relative flex-shrink-0 select-none  ", containerStyles)} style={containerHTMLStyles}>
-
-            <Image
-                draggable={false}
-                src={`/images/${stringify(src)}`}
-                alt={stringify(alt) || ""}
-                fill
-                priority={priority || false}
-                className={cn(
-                    "object-contain flex-shrink-0",
-                    object === "cover" && "object-cover",
-                    object === "contain" && "object-contain",
-                    object === "none" && "object-none",
-                    innerStyles
-                )}
-            />
+        <div className={cn("relative flex-shrink-0 select-none  ", containerStyles)} style={{
+            ...containerHTMLStyles,
+        }}>
+            <AspectRatio ratio={width/height}>
+                <Image
+                    draggable={false}
+                    src={`/images/${stringify(src)}`}
+                    alt={stringify(alt) || ""}
+                    // fill
+                    width={width}
+                    height={height}
+                    priority={priority || false}
+                    className={cn(
+                        "object-contain flex-shrink-0",
+                        object === "cover" && "object-cover",
+                        object === "contain" && "object-contain",
+                        object === "none" && "object-none",
+                        innerStyles
+                    )}
+                />
+            </AspectRatio>
         </div>
     );
 }
