88321c1e0b7478eedd334054d6f3be09b74ac6d3
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-10-06 17:01:16 +0000

    perf: export library

--- src/components/containers/index.ts
+++ src/components/containers/index.ts
@@ -1,1 +1,3 @@
-export { default as GridXOverflowContainer} from "./grid-x-overflow-container"
+export { default as GridXOverflowContainer } from "./grid-x-overflow-container";
+export {default as HeroContainer} from "./hero-container"
+export {default as MapContainer} from "./map-container"
