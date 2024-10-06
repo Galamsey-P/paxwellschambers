4c88bfa3a30135a1f4a99a2b01eb2b093f95cb1a
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-10-06 16:59:54 +0000

    style: whitespace

--- src/components/navigation/logo/index.tsx
+++ src/components/navigation/logo/index.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { ClassValue } from "clsx";
 import { LogoVariant, logoVariantMap } from "./index.types";
 import { cn } from "@/lib/utils";
@@ -15,12 +16,12 @@ export default function Logo({ className, variant }: Props) {
     const { logoVariant } = useNavigation();
 
     return (
-        <Link href="/">
-            <div className={cn("w-[30px] md:w-[40px] 2xl:w-[52px]", className)}>
-                <AspectRatio ratio={52 / 78}>
-                    <Image src={`/images/${logoVariantMap[variant || logoVariant]}`} alt={`${logoVariant} logo`} width={52} height={78} className={cn("", className)} priority />
-                </AspectRatio>
-            </div>
-        </Link>
+            <Link href="/">
+                <div className={cn("w-[30px] md:w-[40px] 2xl:w-[52px]", className)}>
+                    <AspectRatio ratio={52 / 78}>
+                        <Image src={`/images/${logoVariantMap[variant || logoVariant]}`} alt={`${logoVariant} logo`} width={52} height={78} className={cn("", className)} priority />
+                    </AspectRatio>
+                </div>
+            </Link>
     );
 }
