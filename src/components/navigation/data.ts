674cfd63f90b0c94e19174ad9136dce3d7bbd6f9
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-10-07 22:52:42 +0000

    chore: reoraganize data

--- src/components/navigation/data.ts
+++ src/components/navigation/data.ts
@@ -20,14 +20,14 @@ export const TITANS = [
         name: "DUBIK YAKUBU MAHAMA ESQ.",
         position: "Managing Partner",
         education: "BA. Ed./MSC Planning/LLB/BL",
-        description: "Some long description",
+        description: "Called to the bar nearly 30 years ago, He has represented many clients in various cases across the country. He has partnered foreign firms to pursue cases in Ghana from corporate to family law. As a former Chief Prosecutor and Assistant Commissioner of the then Internal Revenue Service of Ghana, now Ghana Revenue Authority, he has extensive knowledge and years of practice in taxation and corporate law practice. He speaks fluent French",
     },
     {
         image: "titan2.jpeg",
         name: "AKROFI KUMOJI ESQ.",
         position: "Senior Partner",
         education: "BA/MBA Finance & Intl. Business/LLB/BL",
-        description: "Some long description",
+        description: "With extensive number of years of practice under his belt, He has extensive knowledge in corporate practice with many leading international non governmental organisations.He has broad knowledge in international law practices and a high analytical mind. He is multilingual and a great team player with extensive foreign education.",
 
     },
     {
@@ -35,7 +35,7 @@ export const TITANS = [
         name: "SAMUEL DUBIK MAHAMA ESQ.",
         position: "Partner",
         education: "BA/LLB/BL",
-        description: "Some long description",
+        description: "An affable lawyer with nearly a decade of experience in handling corporate clients within West Africa and North America. He has extensive knowledge in the international law space and a sizeable number of foreign company clients. He thrives on perfection and is a keen team player.",
 
     },
     {
@@ -43,7 +43,7 @@ export const TITANS = [
         name: "EMMANUEL L. DUBIK MAHAMA ESQ.",
         position: "Partner",
         education: "BA/LLB/BL",
-        description: "Some long description",
+        description: "A young lawyer with extensive knowledge of the law and productive years of practice under his belt. He handles corporate clients to divorce cases and pro bono matters. He uses ADR techniques to achieve swift solutions to cases. He handles financial institutions under his portfolio at the firm.",
 
     },
 ]
