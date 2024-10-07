d4b61d2e5523976c68e8807e994c3d92ba280fc8
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-10-07 22:52:24 +0000

    style: play around with responsiveness

--- src/components/cards/practice-card.tsx
+++ src/components/cards/practice-card.tsx
@@ -7,10 +7,10 @@ type Props = {
 
 export default function PracticeCard({ label}: Props) {
     return (
-        <Card className="w-full bg-[#1e1e1e] hover:bg-deep-sb text-whyte rounded-none h-max pb-0 my-auto cursor-default">
-            <CardContent className=" flex items-center gap-4 pb-0 md:p-5  p-2 my-auto  ">
+        <Card className="w-full max-w-full overflow-hidden bg-[#1e1e1e] hover:bg-deep-sb text-whyte rounded-none h-max pb-0 my-auto cursor-default">
+            <CardContent className=" flex flex-wra items-center gap-2 md:gap-4 pb-0 md:p-5  p-2 my-auto  ">
                 <Gavel className="flex-shrink-0 max-sm:size-4" />
-                <p className="flex-shrink-0">
+                <p className="flex-wrap text-xs lg:text-base">
                     {label}
                 </p>
             </CardContent>
